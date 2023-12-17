afdb03bfbc46cf28a16592c8c50b272b573f23be
MelnykovMykyta <melnikovnikita@ukr.net>
2023-12-17 16:56:50 +0200

    Initial Commit

---
+++ SportEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportEvents
+//
+//  Created by Nikita Melnikov on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
