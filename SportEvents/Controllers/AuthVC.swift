f6870defafc1a05433c2733ed184f9a4664971ad
MelnykovMykyta <melnikovnikita@ukr.net>
2023-12-17 19:26:25 +0200

    AuthVC + D (DesignSystem)

--- SportEvents/Controllers/AuthVC.swift
+++ SportEvents/Controllers/AuthVC.swift
@@ -11,9 +11,11 @@ import SnapKit
 
 class AuthVC: UIViewController {
     
+    var signInButton: UIButton!
+    var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupView()
     }
 }
@@ -21,14 +23,72 @@ class AuthVC: UIViewController {
 private extension AuthVC {
     
     func setupView() {
+        
         view.backgroundColor = D.Colors.mainColor
         
-        let label = UILabel()
-        label.text = "GOLOVNA"
-        view.addSubview(label)
+        let athlete = UIImageView()
+        athlete.image = UIImage(named: "AthleteAuthVC")
+        athlete.contentMode = .scaleAspectFit
+        view.addSubview(athlete)
         
-        label.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        let mainLabel = UILabel()
+        mainLabel.text = D.Texts.mainVCLabel
+        mainLabel.numberOfLines = 2
+        mainLabel.font = UIFont(name: "Helvetica", size: 32)
+        mainLabel.textColor = D.Colors.whiteTextColor
+        mainLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(mainLabel)
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = D.Texts.mainVCDesc
+        descriptionLabel.numberOfLines = 5
+        descriptionLabel.font = UIFont(name: "Helvetica-Light", size: 24)
+        descriptionLabel.textColor = D.Colors.greyTextColor
+        descriptionLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(descriptionLabel)
+        
+        let buttonsSV = UIStackView()
+        buttonsSV.axis = .horizontal
+        buttonsSV.spacing = 20
+        buttonsSV.distribution = .fillEqually
+        view.addSubview(buttonsSV)
+        
+        signInButton = UIButton(type: .system)
+        signInButton.setTitle(D.Texts.signInButtonLabel, for: .normal)
+        signInButton.tintColor = D.Colors.whiteTextColor
+        signInButton.backgroundColor = D.Colors.signInButtonColor
+        signInButton.layer.cornerRadius = 12
+        signInButton.layer.masksToBounds = true
+        buttonsSV.addArrangedSubview(signInButton)
+        
+        signUpButton = UIButton(type: .system)
+        signUpButton.setTitle(D.Texts.signUpButtonLabel, for: .normal)
+        signUpButton.tintColor = D.Colors.greyTextColor
+        signUpButton.layer.cornerRadius = 20
+        signUpButton.layer.masksToBounds = true
+        buttonsSV.addArrangedSubview(signUpButton)
+        
+        athlete.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview().inset(D.Constraints.fourtyPoints)
+            $0.height.equalToSuperview().multipliedBy(D.Sizes.halfSize)
+        }
+        
+        mainLabel.snp.makeConstraints {
+            $0.top.equalTo(athlete.snp.bottom).inset(-D.Constraints.twentyPoints)
+            $0.leading.trailing.equalToSuperview().inset(D.Constraints.twentyPoints)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(mainLabel.snp.bottom).inset(-D.Constraints.twentyPoints)
+            $0.leading.trailing.equalToSuperview().inset(D.Constraints.twentyPoints)
+        }
+        
+        buttonsSV.snp.makeConstraints {
+            $0.height.equalTo(D.Sizes.authButtonHeight)
+            $0.top.equalTo(descriptionLabel.snp.bottom).inset(-D.Constraints.fourtyPoints)
+            $0.leading.trailing.equalToSuperview().inset(D.Constraints.twentyPoints)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(D.Constraints.fourtyPoints)
         }
     }
 }
--- SportEvents/Resources/D.swift
+++ SportEvents/Resources/D.swift
@@ -12,5 +12,25 @@ struct D {
     
     struct Colors {
         static let mainColor = UIColor(red: 0.039, green: 0.145, blue: 0.251, alpha: 1)
+        static let whiteTextColor = UIColor(red: 0.941, green: 1, blue: 1, alpha: 1)
+        static let greyTextColor = UIColor(red: 0.435, green: 0.596, blue: 0.722, alpha: 1)
+        static let signInButtonColor =  UIColor(red: 0, green: 0.431, blue: 1, alpha: 1)
+    }
+    
+    struct Texts {
+        static let mainVCLabel = "Останні спортивні новини та івенти"
+        static let mainVCDesc = "Отримайте широкий спектр інформації про спортивні події, розклад матчів, можливість придбання квитків та зручну систему сповіщень щодо останніх новин і результатів улюблених команд."
+        static let signInButtonLabel = "Вхід"
+        static let signUpButtonLabel = "Реєстрація"
+    }
+    
+    struct Sizes {
+        static let halfSize = 0.5
+        static let authButtonHeight = 48
+    }
+    
+    struct Constraints {
+        static let twentyPoints = 20
+        static let fourtyPoints = 40
     }
 }
